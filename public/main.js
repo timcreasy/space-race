7c7f51b375c04f5c825c61e84f843874d8a07f0a
Tim Creasy <timcreasy@me.com>
2016-10-08 14:05:55 -0500

    Emitting only player moves

--- public/main.js
+++ public/main.js
@@ -1,21 +1,27 @@
 'use strict';
 
 const socket = io();
+let player;
 
-let increase = 2.5;
 $(window).keyup(function (evt) {
 		if (evt.which === 32) {
-			$(".ship").css({'margin-bottom': `${increase}rem`})
-			increase = increase + .5
-			socket.emit('player moved', {msg: socket.id});
-			if (increase === 36.5) {
+			$(".ship").css({'margin-bottom': `${player.increase}rem`})
+			player.increase = player.increase + .5
+			socket.emit('player moved', {player: player});
+			if (player.increase === 36.5) {
 				console.log("You win")
 			}
 		}
 })
 
-
-
-socket.on('connect', () => console.dir(socket));
+socket.on('connect', () => {
+	const username = window.location.search.split('=').slice(-1)[0];;
+	socket.emit('player joined', {username});
+});
+socket.on('player found', (data) => {
+	player = data.player;
+});
+socket.on('update player', ({player}) => {
+	console.log(`${player.username} moved - ${player.increase}`);
+});
 socket.on('disconnect', () => console.log(`Socket disconnected`));
-socket.on('player joined', (game) => console.log(game))
